c39470eec060cdcca0ee4f2db77323d6c21f0521
william989 <hwilliam.cy@gmail.com>
2023-05-16 11:30:44 -0500

    first commit

---
+++ src/main/java/pe/gob/contraloria/sisco/models/Order.java
@@ -0,0 +1,58 @@
+package pe.gob.contraloria.sisco.models;
+
+public class Order {
+    private String name;
+    private String cardType;
+    private int discount;
+    private int price;
+    private double monto;
+    private String descripcion;
+
+    public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
